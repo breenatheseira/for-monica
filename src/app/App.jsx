ce01640a783f0504854662d8d49e69525c72505b
Breena <breenatheseira@gmail.com>
2024-08-07 17:07:10 +0800

    Footer is sticky to content/ bottom of the page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
-// import Footer from '../components/Footer';
+import Footer from '../components/Footer.jsx';
 
 export default function App() {
   return (
-    <>
-      <Header />
-        <h1 className='text-3xl font-bold underline'>Hi!</h1>
-      {/* <Footer /> */}
-    </>
+    <div className='flex flex-col h-screen'>
+      <div className='flex-none'>
+        <Header />
+      </div>
+      <div className='grow'>
+        <h1 className='text-3xl font-bold underline'>
+          Hi
+        </h1>
+      </div>
+      <div className='flex-none'>
+        <Footer />
+      </div>
+    </div>
   )
     
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer(){
+  return (
+    <>
+      <div className="px-4">
+        <hr />
+      </div>
+      <footer className="flex justify-center py-2 ">
+        <p>
+          © 2024 Breena Theseira
+        </p>
+      </footer>
+    </>
+  )
+}
