b290f6ccd4646784a9fb7492274f7fddf04bd0e9
Breena <breenatheseira@gmail.com>
2024-08-18 12:41:04 +0800

    Skeleton Login page

--- src/app/routes/Login.jsx
+++ src/app/routes/Login.jsx
@@ -1,13 +1,19 @@
 import Card from '../../components/card/Card.jsx';
+import CardBody from '../../components/card/CardBody.jsx';
 import CardHeading from '../../components/card/CardHeading.jsx';
 import Title from '../../components/card/Title.jsx';
 
 export default Login = () => {
   return (
-    <Card>
-      <CardHeading>
-        <Title text="Welcome :)" />
-      </CardHeading>
-    </Card>
+    <div className='flex flex-col h-screen bg-slate-200'>
+      <Card>
+        <CardHeading>
+          <Title text="Welcome :)" />
+        </CardHeading>
+        <CardBody>
+          <div>Please login to continue.</div>
+        </CardBody>
+      </Card>
+    </div>
   )
 }
